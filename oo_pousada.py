e8ff042bb48f153f70e66e5e7e21116933e69694
SaraMag25 <saramagalhaes242@gmail.com>
2022-12-20 20:52:49 -0300

    coisas novas

---
+++ oo_pousada.py
@@ -0,0 +1,23 @@
+#Lembrando que:
+# snake_case para variáveis, funções e métodos;
+# PascalCase para classes;
+#============================================================
+# Crie um ambiente O.O. completo utilizando classes e métodos:
+# Classes Hospede, Check-in e Check-out e Lista de Hospedes para elas faça:
+# uma estrutura que cadastre o hospede
+# uma estrutura que gere a lista de hospedes
+# uma estrutura que registre o dia da entrada e o dia de saída
+# mostre o valor que será pago pela estadia
+# você deve validar o CPF, o e-mail do hospede
+# o padrão de data 00/00/000 deve ser verificado tanto no check-in quanto no check-out
+# OBS: Faça todas as validações
+# OBS: Fique atento aos impedimentos dos métodos
+# OBS: Faça a impressão dos elementos
+class Hospede :
+    pass
+class CheckIn():
+    pass
+class CheckOut():
+    pass
+class ListaHostedes():
+    pass
